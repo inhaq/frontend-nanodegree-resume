991aedb3336367c05488977cc972a3122f1a89da
Inzimam <p126013@nu.edu.pk>
2017-04-29 00:20:07 +0500

    objects key-values updated

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,57 +5,86 @@ var bio = {
     "name": "Inzimam Ul Haq",
     "role": "Front-End Web Developer",
     "contacts": {
-        "mobile" : "+923466374133",
-         "email" : "hi@inzi.me",
-          "github" : "@inhaq",
-           "facebook" : "enzimam",
-            "location" : "Pakistan"
+        "mobile": "+923466374133",
+        "email": "hi@inzi.me",
+        "github": "@inhaq",
+        "facebook": "enzimam",
+        "location": "Pakistan"
     },
     "welcomeMessage": "Hi, I'm Inzimam",
-    "skills": ["JavaScript", "HTML5", "CSS3", "JQuery", "Git"],
+    "skills": [
+        "JavaScript", "HTML5", "CSS3", "JQuery", "Git"
+    ],
     "biopic": "images/fry.jpg",
     // "display": function taking no parameters
 };
 
-var education={
- "schools": { 
-        "name": "string",
-         "location": "string",
-         "degree": "string",
-         "majors": "array of strings",
-         "dates": "string (works with a hyphen between them)",
-         "url": "string"
-        },
-        
-    "onlineCourses": {
-        "title": "string",
-         "school": "string",
-         "dates": "string (works with a hyphen between them)",
-         "url": "string"
+var education = {
+    "schools": [
+        {
+            "name": "FAST-NUCES",
+            "location": "Pakistan",
+            "degree": "BS-CS",
+            "majors": [
+                "Data Structures", "Algorithms", "Artificial Intelligence"
+            ],
+            "dates": "2012 - 2016",
+            "url": "http://www.nu.edu.pk/"
+        }, {
+            "name": "GIKI",
+            "location": "Pakistan",
+            "degree": "Pre-Engineering",
+            "majors": [
+                "Maths", "Physics", "Chemistry"
+            ],
+            "dates": "2010 - 2012",
+            "url": "http://www.giki.edu.pk/"
         }
-         
+    ],
+    "onlineCourses": [
+        {
+            "title": "Front-End NanoDegree",
+            "school": "Udacity",
+            "dates": "2017 - Present",
+            "url": "https://www.udacity.com/"
+        }, {
+            "title": "Full Stack JavaScript Development",
+            "school": "FreeCodeCamp",
+            "dates": "2017 - Present",
+            "url": "https://www.freecodecamp.com/"
+        }
+    ],
+
     // "display": function taking no parameters
 };
 
-var work={
-    jobs: {
-         "employer": "string", 
-         "title": "string", 
-         "location": "string", 
-         "dates": "string (Can be 'in progress')",
-         "description": "string" 
-    },
-         
+var work = {
+    "jobs": [
+        {
+            "employer": "CreativeXLab",
+            "title": "Junior Web Developer",
+            "location": "Pakistan",
+            "dates": "May 2016 - Present",
+            "description": "string"
+        }, {
+            "employer": "Self-Employed",
+            "title": "WordPress Expert",
+            "location": "Pakistan",
+            "dates": "April 2015 - Present",
+            "description": "string"
+        }
+    ],
+
     // display: function taking no parameters
 };
 
-var projects={
-          projects: {
-          "title": "string", 
-          "dates": "string (works with a hyphen between them)",
-          "description": "string",
-          "images": "array with string urls"
-          },
-          
+var projects = {
+    "projects": {
+        "title": "Visualization of Air Pollution",
+        "dates": "Dec 2015 - Dec 2016",
+        "description": "Designed a Web Application that visualize the data of the pollution that is Carbon mono-oxide, Smoke, Dust and Noise. For this project we also built a device of Arduino and GPS with required sensors. Data was displayed in Google Maps via Google Map API",
+        "images": "197x148.gif"
+    },
+
     // display: function taking no parameters
 };
