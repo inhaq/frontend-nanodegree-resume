d7c48ea774cc389242708c2ac853a8869c31a14d
Inzimam <p126013@nu.edu.pk>
2017-04-28 23:01:08 +0500

    education object added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,19 +1,34 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var bio={
-     "name" : "Inzimam Ul Haq",
-    "role" : "Front-End Web Developer",
-    "contacts" : [ 
-        "mobile": string,
-          "email": string ,
-          "github": string,
-          "twitter": string (optional),
-          "location": string
-          ]
-         
-    "welcomeMessage": string ,
-    "skills": array of strings,
-    "biopic": string url,
+var bio = {
+    "name": "Inzimam Ul Haq",
+    "role": "Front-End Web Developer",
+    "contacts": {
+        "mobile" : "+923466374133",
+         "email" : "hi@inzi.me",
+          "github" : "@inhaq",
+           "facebook" : "enzimam",
+            "location" : "Pakistan"
+    },
+    "welcomeMessage": "Hi, I'm Inzimam",
+    "skills": ["JavaScript", "HTML5", "CSS3", "JQuery", "Git"],
+    "biopic": "images/fry.jpg",
+    // "display": function taking no parameters
+};
+
+var education={
+ "schools": array of objects with
+         "name": string
+         "location": string
+         "degree": string
+         "majors": array of strings
+         "dates": string (works with a hyphen between them)
+         "url": string
+    "onlineCourses": array of objects with
+         "title": string
+         "school": string
+         "dates": string (works with a hyphen between them)
+         "url": string
     "display": function taking no parameters
-}
+};
