d3e2df47b08729f53493578b84504211b5161678
Inzimam <p126013@nu.edu.pk>
2017-04-28 22:50:45 +0500

    bio data added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio={
+     "name" : "Inzimam Ul Haq",
+    "role" : "Front-End Web Developer",
+    "contacts" : [ 
+        "mobile": string,
+          "email": string ,
+          "github": string,
+          "twitter": string (optional),
+          "location": string
+          ]
+         
+    "welcomeMessage": string ,
+    "skills": array of strings,
+    "biopic": string url,
+    "display": function taking no parameters
+}
